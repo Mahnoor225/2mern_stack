6953d50ba3e4d59774ba6485b00d32cc15b6531f
Mahnoor Mehboob <mahnoormehboob225@gmail.com>
2024-08-02 22:52:51 -0700

    create userSchema file

---
+++ Backend/Model/Authmodel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique:true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+      },
+      isAdmin: {
+        type: Boolean,
+        default: false,
+      },
+    profilepic: {
+        type: String,
+    }
+}, {timesStamps:true});
+
+const userModel = mongoose.model("user",userSchema)
+
+export default userModel
